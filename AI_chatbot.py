9ef4c21333628d5f1f441fd1009edb11a5d86d58
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2025-03-28 11:06:59 +0700

    Update AI_chatbot.py

--- AI_chatbot.py
+++ AI_chatbot.py
@@ -1,7 +1,6 @@
 from huggingface_hub import login
 from transformers import pipeline, BitsAndBytesConfig
 import torch
-import openai
 
 # def generate_text(prompt, chat_history, model="gpt-4-turbo"):
 #     messages = [{"role": "system", "content": "You are a helpful assistant."}]
