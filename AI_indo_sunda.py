8d2a24d42c625dcbdd98259b4f3d1eb815143b4f
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2025-04-06 12:32:54 +0700

    Update AI_indo_sunda.py

--- AI_indo_sunda.py
+++ AI_indo_sunda.py
@@ -1,45 +1,14 @@
-from huggingface_hub import login
-from transformers import pipeline, BitsAndBytesConfig
-import torch
+# ========== AI_indo_sunda.py ==========
 import openai
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# def generate_text(prompt, model="gpt-4-turbo"):
-#     response = openai.ChatCompletion.create(
-#         model=model,
-#         messages=[
-#             {"role": "system", "content": "You are a helpful assistant."},
-#             {
-#                 "role": "user",
-#                 "content": f"Terjemahkan dalam Bahasa Sunda Loma, Tampilkan Hasil terjemahannya saja. Jika menurutmu kata kata yang kamu keluarkan ada bahasa indonesia atau bukan bahasa sunda tolong berikan tanda italic: {prompt}",
-#             },
-#         ],
-#         temperature=0.1,
-#     )
-#     return response["choices"][0]["message"]["content"]
-
-
-def generate_text(prompt, pipe):
-    print("Model berhasil dimuat dengan 4-bit quantization menggunakan BitsAndBytes!")
-
-    messages = [
-        {
-            "role": "system",
-            "content": [{"type": "text", "text": "You are a helpful assistant."}],
-        },
-        {
-            "role": "user",
-            "content": [
-                {"type": "image", "url": "images/ss.png"},
-                {
-                    "type": "text",
-                    "text": f"""abaikan gambar yang saya kirim. Terjemahkan dalam Bahasa Sunda Loma, 
-                    Tampilkan Hasil terjemahannya saja. Jika menurutmu kata kata yang kamu keluarkan 
-                    ada bahasa indonesia atau bukan bahasa sunda tolong berikan tanda italic: {prompt}
-                    """,
-                },
-            ],
-        },
-    ]
-
-    output = pipe(text=messages, max_new_tokens=512)
-    return output[0]["generated_text"][-1]["content"]
+def generate_text(prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"Terjemahkan dalam Bahasa Sunda Loma: {prompt}"}
+        ]
+    )
+    return response.choices[0].message.content
